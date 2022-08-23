e8824a5ed3ee50edcc891efe089f113956bc4fb9
Lucas <lucaswoodyard@yahoo.com>
2022-08-22 21:04:42 -0500

    commit tree d7c5228e77d7de6bf70ab8de8ba8d45811756a9c

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+    let foundIt = false
+  search:
+  for (i = 0;i < array.length;i++) {
+    let first = array[i];
+    
+    for (j = i + 1;j < array.length;j++) {
+        let second = array[j];
+        console.log(`'${first}, this is first'`, `'${second}, this is second'`)
+       
+        if ((first + second) === target) {
+          console.log(first + second)
+          foundIt = true
+          break search
+        }  
+        // else if 
+        //   ((first + second) != target) {
+        //   return false 
+        // }
+       
 }
-
+  }
+  return foundIt
+}
 /* 
   Write the Big O time complexity of your function here
 */
 
 /* 
   Add your pseudocode here
+
+  Take array input from argument and iterate over it adding the first index to the second index.
+  Compare each sum of iteration to target number.
+  If sum = target number return true.
+  Else return false. 
 */
 
 /*
@@ -25,10 +51,15 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
-
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([22, 9, 4, 6, 9], 18));
+  
+  
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 2, 5, 7], 15));
 }
 
 module.exports = hasTargetSum;
